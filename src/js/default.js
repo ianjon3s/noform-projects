08ec6187865b7add4dfc8921442a6ef5c3690418
Ian Jones <ian.jones@substrakt.com>
2021-05-05 16:44:32 +0100

    ADD: image dimensions, console text

--- src/js/default.js
+++ src/js/default.js
@@ -1,1 +1,3 @@
 /** @format */
+
+console.log("%c enjoy yourself!","font-weight: bold; padding-bottom: 0.5em; padding-right: 0.5em; font-size: 40px;color: red; text-shadow: 2px 2px 0 rgb(217,31,38) , 4px 4px 0 rgb(226,91,14) , 6px 6px 0 rgb(245,221,8) , 8px 8px 0 rgb(5,148,68) , 10px 10px 0 rgb(2,135,206) , 12px 12px 0 rgb(4,77,145) , 14px 14px 0 rgb(42,21,113)");
