1a8fe0ff33bfeb57ad5f7adf5dafc42cebad619f
Ian Jones <hello@ianjo.uk>
2023-03-15 22:16:59 +0000

    ADD: stereokinetic project

---
+++ src/js/stereo.js
@@ -0,0 +1,11 @@
+$('.o-input-group--shape input:radio').click(function () {
+            
+    $('#stereo').attr('data-shape',this.value);
+
+});
+
+$('.o-input-group--speed input:radio').click(function () {
+            
+    $('#stereo').attr('data-speed',this.value);
+
+});
